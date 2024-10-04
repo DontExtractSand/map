f8cdfa0489c30784701cec237eb76bbcbb3ab805
DontExtractSand <johann+github@pintiaux.com>
2024-10-04 21:32:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,9 @@ if df is not None:
 
             # Extract the Twitter handle from the matching row
             twitter_handle = matching_row['twitter'].values[0]
+            facebook_handle = matching_row['facebook'].values[0]
 
-            # Check if a Twitter handle is available
+            ### Twitter Message ###
             if pd.notna(twitter_handle) and twitter_handle != '':
                 # Build the tweet message
                 tweet_message = f"@{twitter_handle} Sauf erreur de notre part, vous avez été élu·e grâce aux voix du front républicain. Il est encore temps d’en être digne. Votez la censure contre ce Gouvernement qui remet en cause l’état de droit et continuera la casse des services publics."
@@ -54,6 +55,27 @@ if df is not None:
                 st.markdown(f"[Click here to Tweet]({twitter_url})")
             else:
                 st.write("This député does not have a Twitter handle.")
+            
+            ### Facebook Message ###
+            if pd.notna(facebook_handle) and facebook_handle != '':
+                # Build the Facebook message
+                facebook_message = f"""
+                Le 7 juillet dernier, un formidable élan s’est levé dans le pays pour battre dans les urnes l’extrême-droite, ses idées et son programme.
+
+                Pourtant, Emmanuel Macron refuse d’entendre l’appel des urnes. Pire, il s’entête et forme un gouvernement sans légitimité politique ou populaire, en nommant Michel Barnier, dont le gouvernement réactionnaire perpétuera une politique de mépris et de casse sociale, grâce à la bienveillance du RN. Les propos scandaleux du ministre de l’Intérieur Retailleau sur l’immigration et sur l’Etat de droit en sont les premières preuves. 
+
+                @{facebook_handle}, montrez-vous digne du front républicain, et refusez l’alliance de fait entre Emmanuel Macron, la droite radicalisée et l’extrême-droite. Votez la censure.
+                """
+
+                # Generate the Facebook share URL
+                facebook_url = f"https://www.facebook.com/sharer/sharer.php?u=https://yourwebsite.com&quote={facebook_message}"
+
+                # Display the link to post on Facebook
+                st.write("Click the link below to post the following message on Facebook:")
+                st.write(f"**Facebook Message**: {facebook_message}")
+                st.markdown(f"[Click here to Post on Facebook]({facebook_url})")
+            else:
+                st.write("This député does not have a Facebook handle.")
         else:
             st.write("No matching député found.")
 else:
